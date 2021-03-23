3598cd9be42d07fc5cc62f2a7d72cf333da4b04f
zigod <ziga.meza109@gmail.com>
2021-03-23 21:22:20 +0100

    Design za dodajanje avtov in dodajanje oglasov + file chooser za sliko pri avtih

--- src/main/java/Mainpage.java
+++ src/main/java/Mainpage.java
@@ -4,33 +4,59 @@ import javax.swing.JScrollPane;
 
 public class Mainpage {
     private JPanel main;
-    private JTabbedPane tabbedPane1;
-    private JScrollPane scrollmain;
-    private JTable tabela;
+
+    //Elementi za dodajanje oglasov
+    private JTabbedPane dodajOglasPane;
+    private JFormattedTextField cenaTextField;
+    private JComboBox krajBox;
+    private JFormattedTextField naslovTextField;
+    private JComboBox avtoBox;
+
+    //Elementi za dodajanje avta v Profilu
+    private JFormattedTextField letnikTextField;
+    private JFormattedTextField kwTextField;
+    private JFormattedTextField ccmTextField;
+    private JFormattedTextField KMTextField;
+    private JComboBox znamkaBox;
+    private JComboBox modelBox;
+    private JFormattedTextField opisTextField;
+    private JButton dodajanjeSlikeButton;
+    private JButton dodajOglasButton;
+
+
 
     public Mainpage(int id)
-{
-    JFrame frame = new JFrame("RentACar");
-    String[][] data = {
-            { "test", "test", "test","test" },
-            { "test", "test", "test","test" }
-    };
-    String[] columnNames = { "Cena", "Kraj", "Slika","Oglej"};
-    tabela = new JTable(data, columnNames);
-    tabela.setBounds(30, 40, 200, 300);
+    {
+        JFrame frame = new JFrame("RentACar");
+        frame.setContentPane(main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(300, 300);
+        frame.setVisible(true);
 
-    JScrollPane sp = new JScrollPane(tabela);
-    frame.add(sp);
-    frame.setSize(500, 500);
+        //System.out.print(id);
+
+        setActionListeners();
+    }
+
+    private void setActionListeners() {
+        //login button on click
+        dodajOglasButton.addActionListener(e -> {
 
 
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.pack();
+        });
 
-    frame.setVisible(true);
-    System.out.print(id);
+        dodajanjeSlikeButton.addActionListener(e -> {
+            // Using this process to invoke the contructor,
+            // JFileChooser points to user's default directory
+            JFileChooser j = new JFileChooser();
 
-}
+            // Open the save dialog
+            j.showSaveDialog(null);
+
+
+        });
+    }
 
 
 }
