bc1ff0b39fe37604fd5450429d1f947931188936
zigod <ziga.meza109@gmail.com>
2021-03-23 14:28:53 +0100

    Popravek test wololo

--- src/main/java/login.java
+++ src/main/java/login.java
@@ -27,11 +27,11 @@ public class login {
             String email = emailField.getText();
             char[] password = passwordField.getPassword()       ;
 
-            String pass = new String(password);
+            //String pass = new String(password);
             //System.out.print(username);
-            boolean b = baza.SelectLogin(email, pass);
+            //boolean b = baza.SelectLogin(email, pass);
             //System.out.print(email);
-            System.out.print(b);
+            //System.out.print(b);
         });
 
     }
